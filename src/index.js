4c0d095800114067cf379e3448ef073e39be5c9f
Dmitry G. Anderson <dandgerson@gmail.com>
2021-10-02 01:18:13 +0300

    move the phrase

--- src/index.js
+++ src/index.js
@@ -14,8 +14,55 @@ const scene = {
     this.load.bitmapFont('arcade', arcadePng, arcadeXml)
   },
   create() {
-    this.add.bitmapText(config.width / 2, config.height / 2, 'arcade', 'Hello Phaser').setOrigin(0.5)
+    this.helloText = this.add.bitmapText(
+      config.width / 2,
+      config.height / 2,
+      'arcade',
+      'Hello Phaser'
+    ).setOrigin(0.5)
+
+    const self = this
+    console.log({
+      key: self.input.keyboard.key,
+    })
+
+    this.cursors = this.input.keyboard.createCursorKeys()
   },
+  update() {
+    switch (true) {
+      case this.cursors.left.isDown: {
+        this.helloText.x -= 10
+        break
+      }
+      case this.cursors.right.isDown: {
+        this.helloText.x += 10
+        break
+      }
+      case this.cursors.up.isDown: {
+        this.helloText.y -= 10
+        break
+      }
+      case this.cursors.down.isDown: {
+        this.helloText.y += 10
+      }
+      default: break
+    }
+
+    if (this.helloText.x > config.width + this.helloText.width / 2) {
+      this.helloText.x = -this.helloText.width / 2
+    }
+    if (this.helloText.x < -this.helloText.width / 2) {
+      this.helloText.x = config.width + this.helloText.width / 2
+    }
+    if (this.helloText.y > config.height + this.helloText.height / 2) {
+      this.helloText.y = -this.helloText.height / 2
+    }
+    if (this.helloText.y < -this.helloText.height / 2) {
+      this.helloText.y = config.height + this.helloText.height / 2
+    }
+
+
+  }
 }
 
 const phaserConfig = {
