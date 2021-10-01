c2a241e3391ffa6a432f09b004e2b62b334162a7
Dmitry G. Anderson <dandgerson@gmail.com>
2021-10-02 02:53:45 +0300

    draw room

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Phaser from "phaser";
 import arcadePng from 'font/arcade.png'
 import arcadeXml from 'font/arcade.xml'
+import tiles from 'assets/colored.png'
 
 import 'styles/main.scss'
 
@@ -12,7 +13,18 @@ const config = {
 const scene = {
   preload() {
     this.load.bitmapFont('arcade', arcadePng, arcadeXml)
+
+    this.load.spritesheet(
+      'tiles',
+      tiles,
+      {
+        frameWidth: 16,
+        frameHeight: 16,
+        spacing: 1,
+      }
+    )
   },
+
   create() {
     this.helloText = this.add.bitmapText(
       config.width / 2,
@@ -21,13 +33,64 @@ const scene = {
       'Hello Phaser'
     ).setOrigin(0.5)
 
-    const self = this
-    console.log({
-      key: self.input.keyboard.key,
-    })
-
     this.cursors = this.input.keyboard.createCursorKeys()
+
+    const tileMap = {
+      0: {
+        text: 'floor',
+        num: 0,
+      },
+      1: {
+        text: 'wall',
+        num: 554,
+      },
+      'S': {
+        text: 'skeleton',
+        num: 221,
+      },
+      'I': {
+        text: 'pillar',
+        num: 424,
+      }
+    }
+
+    const level = [
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 'I', 0, 0, 0, 0, 'I', 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 'S', 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 'I', 0, 0, 0, 0, 'I', 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    ].map(row => row.map(tile => tileMap[tile].num))
+
+
+
+    const tileSize = 16
+    const tileConfig = {
+      data: level,
+      tileWidth: tileSize,
+      tileHeight: tileSize,
+    }
+
+    const map = this.make.tilemap(tileConfig)
+    const tileset = map.addTilesetImage(
+      'tiles',
+      'tiles',
+      tileSize,
+      tileSize,
+      0,
+      1,
+    )
+
+    console.log({ tileset })
+
+    const ground = map.createLayer(0, tileset, 0, 0)
   },
+
   update() {
     switch (true) {
       case this.cursors.left.isDown: {
